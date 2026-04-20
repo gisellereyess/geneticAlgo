9fcbf85097bc56c52581789442b8c6bb0b055122
Parker Robinson <119458723+ParkerRobinson285@users.noreply.github.com>
2026-04-20 11:49:58 -0500

    Changed section implementation
    Will access what section a class is by where it is in the list.
    (i.e. first class is SLA101A, second SLA101B, etc.)

--- fit.py
+++ fit.py
@@ -3,11 +3,11 @@ import Chromosome
 from data import classes as classes_data
 from data import rooms as rooms_data
 from data import teachers as teachers_data
+from data import sections as sections_data
 
 def fit(schedules):
     #iterates through each schedule to be evaluated
     for schedule in schedules:
-        sections = schedule.get_section_list()
         teachers = schedule.get_teacher_list()
         times = schedule.get_time_list()
         rooms = schedule.get_room_list()
@@ -23,13 +23,13 @@ def fit(schedules):
         #removes classes as it goes to prevent doubling comparisons
         #tracks remaining classes by watching length of teachers list
         while(len(teachers) > 1):
-            section = sections.pop()
+            section = len(sections_data) - len(teachers)
             teacher = teachers.pop()
             time = times.pop()
             room = rooms.pop()
             
             #room sizes
-            class_size = classes_data[section][0]
+            class_size = classes_data[sections_data[section]][0]
             if(rooms_data[room] < class_size):                              #rooms is smaller than class
                 score -= 0.5
             elif(rooms_data[room] > class_size * 1.5):                      #room too big for class
@@ -40,9 +40,9 @@ def fit(schedules):
                 score += 0.3
 
             #preferred facilitators
-            if(teacher in classes_data[section][1]):                        #preferred facilitator
+            if(teacher in classes_data[sections_data[section]][1]):                        #preferred facilitator
                 score += 0.5
-            elif(teacher in classes_data[section][2]):                      #listed facilitator
+            elif(teacher in classes_data[sections_data[section]][2]):                      #listed facilitator
                 score += 0.2
             else:                                                           #unlisted facilitator
                 score -= 0.1

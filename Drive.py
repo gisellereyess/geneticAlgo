6a2a3bee336957851a74d94215d721ed356f448e
Parker Robinson <parkernumberfive@gmail.com>
2026-04-21 18:30:57 -0500

    Fix Final

--- Drive.py
+++ Drive.py
@@ -1,6 +1,7 @@
 from Chromosome import Chromosome
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 from fit import fit
 from Reproduction import make_kids
 POP_SIZE = 300
@@ -11,22 +12,27 @@ def drive():
   change = 0
   mutation_rate = 0.1
   for i in range(GENERATIONS):
+    #get fitness
     fit(generation)
-    gen_fitness = list(map(lambda x: x.get_fitness(), generation))
-    for j in range(len(generation)):
-      generation[j].set_fitness(gen_fitness[j])
-    best_ind = gen_fitness.index(max(gen_fitness))
+    #put fitnesses into list
+    raw_fitness = list(map(lambda x: x.get_fitness(), generation))
+    #find the best schedule
+    best_ind = raw_fitness.index(max(raw_fitness))
     best = generation[best_ind]
-    worst_ind = gen_fitness.index(min(gen_fitness))
+    #find the worst schedule
+    worst_ind = raw_fitness.index(min(raw_fitness))
     worst = generation[worst_ind]
-    avg = sum(gen_fitness) / len(gen_fitness)
+    #compute average from raw fitnesses
+    avg = sum(raw_fitness) / len(raw_fitness)
     fitnesses.append(avg)
+    #find change in average fitness
     if(i > 0):
-      change = fitnesses[-1] - fitnesses[-2] / abs(fitnesses[-2])
+      change = (fitnesses[-1] - fitnesses[-2]) / abs(fitnesses[-2])
+    #half the mutation rate if we're over 50 generations and increasing average fitness
     if(i > 50 and change > 0):
       mutation_rate /= 2
-    print(f'Best: {best.get_fitness()}')
     print(best)
+    print(f'Best: {best.get_fitness()}')
     print(f'Worst: {worst.get_fitness()}')
     print(f'Average Improvement: {change}')
     generation = make_kids(generation, mutation_rate)
